a6896c2985cc306fe3fd42a91523ac25668265dc
rachel <rachelkiffiak1@gmail.com>
2020-08-30 23:22:10 -0600

    Completed the functions with three calls, as per assignment

---
+++ js/app.js
@@ -0,0 +1,35 @@
+function sum(num1, num2, num3) {
+    return num1 + num2 * 5;
+}
+console.log(sum(2,5));
+console.log(sum(10,8));
+console.log(sum(12, 3));
+
+function numberstring(argument1){
+    if(argument1.length > 10){
+        return true;
+    }else if(argument1.length <= 10){
+        return false;
+    }else{
+        console.log("There is an error");
+    }
+}
+
+console.log(numberstring("Abominable"));
+console.log(numberstring("flower"));
+console.log(numberstring("Supercalifragaliciousexpealidocious"));
+
+
+function letterfind(){
+    
+    for(var i=0; i<=letterfind.length; i++) {
+        if(letterfind[i].startswith('ph')) {
+            return([i]);
+        }
+    }
+    console.log("Does not start with ph");
+}
+
+console.log(letterfind("Phaeton", "parrot"));
+console.log(letterfind("flour", "phantom"));
+console.log(letterfind("phalanx", "electricity"));
